7632480107c8ba87e949ba6e26f4b7d4a1be18c4
hfercc <hfercxx@hotmail.com>
2020-01-04 23:44:32 +0800

    Update structure

--- controller.py
+++ controller.py
@@ -5,13 +5,16 @@ import torch.nn.functional as F
 import torch.nn.utils.rnn as rnnutils
 
 import numpy as np
+import torchvision
 
-from utils import *
+from utils import copy_and_pad, preprocess
 from optimizers import Optimizer
+from collections import defaultdict
 
 class LSTMCoordinator(nn.Module):
 
-    def __init__(self, step_increment = 200, max_length = 0, hidden_layers = 2, hidden_units = 20, input_dim = 2, num_actions = 3):
+    def __init__(self, step_increment = 200, max_length = 0, hidden_layers = 2, hidden_units = 20, input_dim = 2, \
+        num_actions = 3):
         super().__init__()
 
         self.hidden_layers = hidden_layers
@@ -20,35 +23,48 @@ class LSTMCoordinator(nn.Module):
 
         self.lstm = torch.nn.LSTM(self.input_dim, self.hidden_units, self.hidden_layers)
         self.max_length = max_length
-        self.decoder = nn.Linear(self.max_length * self.hidden_units, num_actions)
+        self.decoder = nn.Linear(2 * self.hidden_units, num_actions)
     def forward(self, x, hx, cx):
+        
         if (hx is None):
-            hx, cx = self.lstm(x)
+            output, (hx, cx) = self.lstm(x)
         else:
-            hx, cx = self.lstm(x, (hx, cx))
+            output, (hx, cx) = self.lstm(x, (hx, cx))
 
-        lstm_out, _ = rnnutils.pad_packed_sequence(hx, batch_first = True)
-        lstm_out = lstm_out.reshape(lstm_out.shape[0], -1)
+        lstm_out, _ = rnnutils.pad_packed_sequence(output, batch_first = True)
+        lstm_out = hx.reshape(lstm_out.shape[0], -1)
         logit = self.decoder(lstm_out)
         prob = torch.nn.functional.softmax(logit, dim=-1)
 
         action = prob.multinomial(1)
         log_prob = torch.nn.functional.log_softmax(logit, dim=-1)
         selected_log_probs = log_prob.gather(1, action.data)
-
         return action, selected_log_probs, hx, cx    
 
 class Controller(object):
-    def __init__(self, coordinator = None, optimizer = None, alpha = 0.001, length_unroll = 20, optimizers = []):
+    def __init__(self, parameters, coordinator = None, optimizer = None, meta_alpha = 0.001, length_unroll = 20,\
+        alpha = 0.001, beta1 = 0.9, beta2 = 0.999, eta = 1e-8):
         self.coordinator = coordinator
-        self.optimizer = optimizer(self.coordinator.parameters(), lr = alpha)
+        self.optimizer = optimizer(self.coordinator.parameters(), lr = meta_alpha)
         self.length_unroll = length_unroll
         self.hx = None
         self.cx = None
-        self.step = 0
+        self.meta_step = 0
 
         self.selected_log_probs = []
-        self.optimizers = optimizers
+        
+        self.parameters = list(parameters)
+        self.state = defaultdict(dict)
+        
+        self.alpha = alpha
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eta = eta
+        
+        self.update_rules = [lambda x, para: self.alpha * x, \
+            lambda x, para: self.alpha / (self.eta + np.sqrt(self.state[para]['r'])) * x, \
+            lambda x, para: self.alpha * self.state[para]['mt_hat'] / (np.sqrt(self.state[para]['vt_hat']) + self.eta)]
+        
     def reset(self):
         self.hx = None
         self.cx = None
@@ -56,34 +72,59 @@ class Controller(object):
 
     def meta_update(self, gradients, length_gradients):
         inputs = preprocess(gradients)
-        print(inputs.shape)
-        packed_inputs = rnnutils.pack_padded_sequence(inputs, length_gradients, batch_first = True)
+        packed_inputs = rnnutils.pack_padded_sequence(inputs, length_gradients, batch_first = True, enforce_sorted = False)
         action, selected_log_probs, self.hx, self.cx = self.coordinator(packed_inputs, self.hx, self.cx)
 
-        self.action = action.numpy()
+        self.action = action.numpy().flatten()
         self.selected_log_probs.append(selected_log_probs)
-        self.step += 1
-
-    def perform_update(self, parameters, gradients, names):
-        num_parameters = len(self.action)
-        for i in range(num_parameters):
-            print(parameters[i])
-            print(gradients[i])
-            print(names[i])
-            print(self.action[i])
-            self.optimizers(parameters[i], gradients[i], names[i], self.action[i])
+        self.meta_step += 1
+            
+    def step(self):
+        
+        gradients = list(map(lambda x: x.grad.data.view(1, -1), self.parameters))
+        padded_grad, length_grad = copy_and_pad(gradients)
+        with torch.no_grad():
+            self.meta_update(padded_grad, length_grad)
+        count = 0
+        for p in self.parameters:
+            state = self.state[p]
+            if len(state) == 0: # State initialization
+                state['t'] = 0
+                state['mt'] = 0
+                state['vt'] = 0
+                state['mt_hat'] = 0
+                state['vt_hat'] = 0
+                state['r'] = 0
+                
+            grad = p.grad.data
+            state['t'] = state['t'] + 1
+            state['mt'] = self.beta1 * state['mt'] + (1 - self.beta1) * grad
+            state['vt'] = self.beta2 * state['vt'] + (1 - self.beta2) * (grad ** 2)
+            state['mt_hat'] = state['mt'] / (1 - np.power(self.beta1, state['t']))
+            state['vt_hat'] = state['vt'] / (1 - np.power(self.beta2, state['t']))
+            state['r'] = state['r'] + (grad ** 2)
+            p.data.add_(-self.update_rules[self.action[count]](grad, p))
+            print(grad)
+        
+    def zero_grad(self):
+        for p in self.parameters:
+            if p.grad is not None:
+                p.grad.detach_()
+                p.grad.zero_()
+    
 
 if __name__ == '__main__':
     
-    x = torch.randn((10, 20))
-    model = nn.Linear(20, 1)
-    y = model(x)
-    loss = torch.sum(y ** 2)
-    loss.backward()
-    para, grad, names = get_parameters_and_gradients(model)
-    print(names)
-    padded_para, length_para = copy_and_pad(para)
-    padded_grad, length_grad = copy_and_pad(grad)
-    controller = Controller(LSTMCoordinator(max_length = length_grad[0]), optim.Adam, optimizers = Optimizer())
-    controller.meta_update(padded_grad, length_grad)
-    controller.perform_update(para, grad, names)
+    x = torch.randn((1, 3, 224, 224))
+    label = torch.zeros(1).long()
+    model = torchvision.models.AlexNet(2)
+    max_length = max(list(map(lambda x:x.view(1,-1).shape[1], model.parameters())))
+    criterion = nn.CrossEntropyLoss()
+    controller = Controller(model.parameters(), coordinator = LSTMCoordinator(max_length = max_length), optimizer = optim.Adam)
+    for i in range(10):
+        y = model(x)
+        loss = criterion(y, label)
+        loss.backward()
+        controller.zero_grad()
+        controller.step()
+        print(loss)
