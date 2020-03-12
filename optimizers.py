d47aae7a83e152cff47d22eaaba907b7bf2560b8
hfercc <hfercxx@hotmail.com>
2020-03-12 23:31:28 +0800

    p

--- optimizers.py
+++ optimizers.py
@@ -48,7 +48,7 @@ class MixtureOptimizer(object):
             state['vt'] = self.beta2 * state['vt'] + (1 - self.beta2) * (grad ** 2)
             state['mt_hat'] = state['mt'] / (1 - np.power(self.beta1, state['t']))
             state['vt_hat'] = state['vt'] / (1 - np.power(self.beta2, state['t']))
-            layer_index = self.layers.index(name.split([0]))
+            layer_index = self.layers.index(name.split('.')[0])
             p.data.add_(-self.update_rules[self.action[layer_index]](grad, p))
         
     def zero_grad(self):
