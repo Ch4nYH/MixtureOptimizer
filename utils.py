9fac72eb7a30e5bd8bae3ab6bdcb9b3bc2bc8f0e
hfercc <hfercxx@hotmail.com>
2020-03-25 15:54:54 +0800

    p

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 from torchvision import transforms
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
 
 
 def preprocess(x, k = 5):
