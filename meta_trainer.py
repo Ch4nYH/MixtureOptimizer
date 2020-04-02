6baefe0ee76abfae1acf637899b9ef1170941877
hfercc <hfercxx@hotmail.com>
2020-04-02 12:53:30 +0800

    update

--- meta_trainer.py
+++ meta_trainer.py
@@ -234,8 +234,8 @@ class MetaRunner(object):
     def evaluate(self):
         observation, prev_loss, prev_val_loss = self.trainer.observe()
         self.step += self.window_size
-        prev_hidden = torch.zeros_like(self.rollouts.recurrent_hidden_states[0], device = self.rollouts.recurrent_hidden_states.device)
-
+        prev_hidden = torch.zeros_like(self.rollouts.recurrent_hidden_states[0])
+        prev_hidden = prev_hidden.cuda()
         while self.step < self.total_steps:
             for step in range(self.num_steps):
                 with torch.no_grad():
